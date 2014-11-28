995605037fee56b24c71e0e028e0cdeca588dc2f
Dana Scheider <dana.scheider@gmail.com>
2014-11-28 12:53:39 -0800

    Beginnings of the gem

---
+++ lib/reactive_extensions.rb
@@ -0,0 +1,16 @@
+Dir['./lib/reactive_extensions/**/*.rb'].each {|f| require f }
+
+# The ReactiveExtensions module adds useful methods to core Ruby classes. You
+# can use these methods by adding +require 'reactive_extensions'+ to your project.
+# Then, ReactiveExtensions methods can be called on any Ruby object just like the 
+# object's own methods. Easy peasy!
+# 
+# In this example, ReactiveExtension's #try_rescue method is called on an array:
+#     require 'reactive_extensions'   
+#     arr = %w(foo, bar, baz)
+#     bad = Math.pi
+#     arr.try_rescue(:join, '.')   # => 'foo.bar.baz'
+#     nil.try_rescue(:join, '.')   # => nil
+
+module ReactiveExtensions
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'simplecov'
+require 'coveralls'
+require 'rspec'
+
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start if ENV["COVERAGE"]
+Coveralls.wear!
+
+require File.expand_path('../../lib/reactive_extensions.rb', __FILE__)
+
+RSpec.configure do |c|
+  c.order  = 'random'
+end
