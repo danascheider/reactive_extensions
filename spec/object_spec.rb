7df9926c45cc17c5cd6a08c90484b7a4afbf4d7a
Dana Scheider <dana.scheider@gmail.com>
2014-11-29 12:45:30 -0800

    Test #try_rescue when called on Object base class

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Object do 
   describe '#try_rescue method' do 
-    context 'when self is nil' do 
+    context 'called on nil' do 
       it 'returns nil' do 
         expect(nil.try_rescue(:collect) {|i| i + 2 }).to eql nil
       end
@@ -12,11 +12,23 @@ describe Object do
       it 'calls the method' do 
         expect('foo'.try_rescue(:upcase)).to eql 'FOO'
       end
+
+      context 'called on the Object base class' do 
+        it 'calls the method' do 
+          expect((Object.new).try_rescue(:class)).to eql Object
+        end
+      end
     end
 
     context 'when a NoMethodError is raised' do 
       it 'returns nil' do 
         expect(('foo').try_rescue(:bar)).to eql nil
+      end
+
+      context 'when called on the Object base class' do 
+        it 'returns nil' do 
+          expect((Object.new).try_rescue(:foo)).to eql nil
+        end
       end
     end
   end
