a83790385796a5a04f9cb282c7064b9f375aefb0
Dana Scheider <dana.scheider@gmail.com>
2014-11-30 13:38:01 -0800

    Modify directory structure again - experimental

--- version.rb
+++ version.rb
@@ -7,7 +7,7 @@ module ReactiveExtensions
     MAJOR = '0'
     MINOR = '5'
     PATCH = '0'
-    PRE   = 'beta3'
+    PRE   = 'beta4'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].join('.').chomp('.')
   end
