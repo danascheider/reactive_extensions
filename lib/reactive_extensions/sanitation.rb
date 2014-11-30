df5b092dcf0a44f61ee359c0b1251158f891b2c7
Dana Scheider <dana.scheider@gmail.com>
2014-11-29 22:32:26 -0800

    Add #standardize method

---
+++ lib/reactive_extensions/sanitation.rb
@@ -0,0 +1,137 @@
+# This file adds the +#clean+, +#clean!+, +#only+, +#only!+, +#standardize+,
+# and +#standardize!+ methods to Ruby's core Hash class. These methods enable
+# the user to easily add or remove particular keys from a hash.
+
+# Ruby's core Has class. See documentation for version 
+# 2.1.5[http://ruby-doc.org/core-2.1.5/Hash.html],
+# 2.0.0[http://ruby-doc.org/core-2.0.0/Hash.html], or 
+# 1.9.3[http://ruby-doc.org/core-1.9.3/Hash.html].
+
+class Hash
+
+  # The +#clean+ method returns a duplicate of the calling hash with the 
+  # specified +*bad_keys+, if present, removed. If none of the bad keys
+  # are present, +#clean+ will simply return a duplicate hash. 
+  #
+  # +#clean+ is a non-destructive method. The original hash will still be
+  # unchanged after it is called.
+  #
+  # +#clean+ may also be called by the aliases +#not+ and +#except+.
+  #
+  # Examples:
+  #     hash = {:foo => 'bar', :bar => 'baz'}
+  #     hash.clean(:foo)                        # => {:bar => 'baz'}
+  #     hash.not(:foo, :bar)                    # => {}
+  #     hash.except(:bar, :norf)                # => {:foo => 'bar'}
+  #     hash.clean(:norf)                       # => {:foo => 'bar', :bar => 'baz'}
+  #     hash                                    # => {:foo => bar', :bar => 'baz'}
+
+  def clean(*bad_keys)
+    dup = self.reject {|key, value| bad_keys.flatten.include?(key) }
+    dup
+  end
+
+  alias_method :not, :clean
+  alias_method :except, :clean
+
+  # The +#clean!+ method returns a duplicate of the calling hash with the 
+  # specified +*bad_keys+, if present, removed. If none of the bad keys
+  # are present, +#clean!+ will simply return a duplicate hash. 
+  #
+  # +#clean!+ is a non-destructive method. The original hash will still be
+  # unchanged after it is called.
+  #
+  # +#clean!+ may also be called by the aliases +#not!+ and +#except!+.
+  #
+  # Examples:
+  #     hash = {:foo => 'bar', :bar => 'baz'}
+  #     hash.clean!(:foo)                       # => {:bar => 'baz'}
+  #     hash                                    # => {:bar => 'baz'}
+  # 
+  #     hash.not!(:foo, :bar)                   # => {}
+  #     hash                                    # => {}
+  # 
+  #     hash = {:foo => 'bar', :bar => 'baz'}
+  #     hash.except!(:bar, :norf)               # => {:foo => 'bar'}
+  #     hash                                    # => {:foo => 'bar'}
+
+  def clean!(*bad_keys)
+    reject! {|key, value| bad_keys.flatten.include?(key) }
+    self
+  end
+
+  alias_method :not!, :clean!
+  alias_method :except!, :clean!
+
+  # The +#only+ method returns a duplicate of the calling hash with only the 
+  # specified +*good_keys+, if present. If none of the good keys
+  # are present, +#only+ will simply return an empty hash. 
+  #
+  # +#only+ is a non-destructive method. The original hash will still be
+  # unchanged after it is called.
+  #
+  # Examples:
+  #     hash = {:foo => 'bar', :bar => 'baz'}
+  #     hash.only(:foo)                         # => {:foo => 'bar'}
+  #     hash.only(:qux)                         # => {}
+  #     hash                                    # => {:foo => 'bar', :bar => 'baz'}
+
+  def only(*good_keys)
+    dup = self.select {|key, value| good_keys.flatten.include?(key) }
+  end
+
+  # The +#only!+ method returns a duplicate of the calling hash with only the 
+  # specified +*good_keys+, if present. If none of the good keys
+  # are present, +#only!+ will simply return an empty hash. 
+  #
+  # +#only!+ is a destructive method. The original hash will be changed in place
+  # when it is called.
+  #
+  # Examples:
+  #     hash = {:foo => 'bar', :bar => 'baz'}
+  #     hash.only!(:foo)                         # => {:foo => 'bar'}
+  #     hash.only!(:qux)                         # => {}
+  #     hash                                     # => {}
+
+  def only!(*good_keys)
+    select! {|key, value| good_keys.flatten.include?(key) }
+    self
+  end
+
+  # The +#standardize+ method returns a duplicate of the calling hash with
+  # the +*keys+ specified as arguments. Any other keys in the hash will be 
+  # removed. Behavior when adding keys depends on whether the +:errors+
+  # option is set to +true+ or +false+ (default). 
+  #
+  # If the +:errors+ option is set to +false+, the method will add any missing
+  # keys to the hash, populating them with +nil+ values. If +:errors+ is set to 
+  # +true+, then an +ArgumentError+ will be raised unless all values are 
+  # present in the hash already.
+  #
+  # Examples with +:errors => false+:
+  #     hash = {:foo => 'bar', :bar => 'baz'}
+  #     hash.standardize(:foo)                  # => {:foo => 'bar'}
+  #     hash.standardize(:foo, :qux)            # => {:foo => 'bar', :qux => nil}
+  #
+  # Examples with +:errors => true+:
+  #     hash = {:foo => 'bar', :bar 'baz'}
+  #     hash.standardize(:foo, :errors => true)           # => {:foo => 'bar'}
+  #     hash.standardize(:foo, :qux, :errors => true)     # => ArgumentError
+
+  def standardize(*kees)
+    options = kees.pop if kees.last.is_a? Hash
+    dup = deep_dup.only(kees)
+
+    kees.each do |key| 
+      unless dup.has_key? key
+        raise ArgumentError.new("#{self} is missing required key #{key}") if options.try_rescue(:[], :errors) === true
+        dup[key] = nil
+      end
+    end
+
+    dup
+  end
+
+  def standardize!(*keys, errors: false)
+  end
+end
---
+++ spec/hash_spec.rb
@@ -0,0 +1,201 @@
+require 'spec_helper' 
+
+describe Hash do 
+  let(:hash) { { :foo => 'bar', :baz => 'qux' } }
+
+  describe '#clean' do 
+    it 'removes specified keys' do 
+      expect(hash.clean(:baz)).to eql({:foo => 'bar'})
+    end
+
+    it 'is non-destructive' do 
+      expect{ hash.clean(:baz) }.not_to change(hash, :length)
+    end
+
+    it 'ignores absent keys' do 
+      expect{ hash.clean(:raboof) }.not_to raise_error
+    end
+
+    it 'accepts multiple arguments' do 
+      expect(hash.clean(:baz, :raboof, :norf)).to eql({:foo => 'bar'})
+    end
+
+    context 'when all keys are bad' do 
+      it 'returns an empty hash' do 
+        expect(hash.clean(:foo, :baz)).to eql({})
+      end
+    end
+  end
+
+  describe '#clean!' do 
+    it 'removes specified keys' do 
+      expect(hash.clean!(:baz)).to eql({:foo => 'bar'})
+    end
+
+    it 'is destructive' do 
+      expect{ hash.clean!(:baz) }.to change(hash, :length)
+    end
+
+    it 'ignores absent keys' do 
+      expect{ hash.clean!(:raboof) }.not_to raise_error
+    end
+
+    it 'accepts multiple arguments' do 
+      expect(hash.clean!(:baz, :raboof, :norf)).to eql({:foo => 'bar'})
+    end
+
+    context 'when all keys are bad' do 
+      it 'returns an empty hash' do 
+        expect(hash.clean!(:foo, :baz)).to eql({})
+      end
+    end
+  end
+
+  describe '#only' do 
+    it 'retains only listed keys' do 
+      expect(hash.only(:foo)).to eql({:foo => 'bar'})
+    end
+
+    it 'is non-destructive' do 
+      expect{ hash.only(:foo) }.not_to change(hash, :length)
+    end
+
+    it 'accepts multiple arguments' do 
+      expect(hash.only(:foo, :baz)).to eql({:foo => 'bar', :baz => 'qux'})
+    end
+
+    context 'when no keys are allowed' do 
+      it 'returns an empty hash' do 
+        expect(hash.only(:raboof)).to eql({})
+      end
+
+      it 'doesn\'t raise an error' do 
+        expect{ hash.only(:raboof) }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#only!' do 
+    it 'retains only listed keys' do 
+      expect(hash.only!(:foo)).to eql({:foo => 'bar'})
+    end
+
+    it 'is destructive' do 
+      expect{ hash.only!(:foo) }.to change(hash, :keys)
+    end
+
+    it 'accepts multiple arguments' do 
+      expect(hash.only!(:foo, :baz, :norf)).to eql({:foo => 'bar', :baz => 'qux'})
+    end
+
+    context 'when no keys are allowed' do 
+      it 'empties the hash' do 
+        expect(hash.only!(:raboof)).to eql({})
+      end
+
+      it 'doesn\'t raise an error' do 
+        expect{ hash.only!(:raboof) }.not_to raise_error
+      end
+    end
+  end
+
+  describe '#standardize' do 
+    let(:hash) { { :foo => 'bar', :bar => 'baz', :baz => 'qux' } }
+
+    it 'is non-destructive' do 
+      expect{ hash.standardize(:foo, :bar) }.not_to change(hash, :keys)
+    end
+
+    context 'exact match' do 
+      it 'returns the hash as-is' do 
+        expect(hash.standardize(:foo, :bar, :baz)).to eql hash 
+      end
+    end
+
+    context 'keys removed' do 
+      it 'removes keys not given in args' do 
+        expect(hash.standardize(:foo, :baz)).to eql({:foo => 'bar', :baz => 'qux'})
+      end
+    end
+
+    context 'args include keys not present in hash' do 
+      let(:with_nil) { {:foo => 'bar', :bar => 'baz', :baz => 'qux', :norf => nil } }
+
+      context 'default' do 
+        it 'populates with nil values' do 
+          expect(hash.standardize(:foo, :bar, :baz, :norf)).to eql(with_nil)
+        end
+
+        it 'doesn\'t raise an error' do 
+          expect{ hash.standardize(:foo, :bar, :baz, :norf) }.not_to raise_error
+        end
+      end
+
+      context 'with errors option set to false' do 
+        it 'populates with nil values' do 
+          expect(hash.standardize(:foo, :bar, :baz, :norf, :errors => false)).to eql(with_nil)
+        end
+
+        it 'doesn\'t raise an error' do 
+          expect{ hash.standardize(:foo, :bar, :baz, :norf, :errors => false) }.not_to raise_error
+        end
+      end
+
+      context 'with errors option set to true' do 
+        it 'raises an error' do 
+          expect{ hash.standardize(:foo, :bar, :baz, :norf, :errors => true) }.to raise_error(ArgumentError)
+        end
+      end
+    end
+  end
+
+  describe '#standardize!' do 
+    let(:hash) { { :foo => 'bar', :bar => 'baz', :baz => 'qux' } }
+
+    it 'is destructive' do 
+      expect{ hash.standardize!(:foo, :bar) }.to change(hash, :keys)
+    end
+
+    context 'exact match' do 
+      it 'returns the hash as-is' do 
+        expect(hash.standardize!(:foo, :bar, :baz)).to eql hash 
+      end
+    end
+
+    context 'keys removed' do 
+      it 'removes keys not given in args' do 
+        expect(hash.standardize!(:foo, :baz)).to eql({:foo => 'bar', :baz => 'qux'})
+      end
+    end
+
+    context 'args include keys not present in hash' do 
+      let(:with_nil) { {:foo => 'bar', :bar => 'baz', :baz => 'qux', :norf => nil } }
+
+      context 'default' do 
+        it 'populates with nil values' do 
+          expect(hash.standardize!(:foo, :bar, :baz, :norf)).to eql(with_nil)
+        end
+
+        it 'doesn\'t raise an error' do 
+          expect{ hash.standardize!(:foo, :bar, :baz, :norf) }.not_to raise_error
+        end
+      end
+
+      context 'with errors option set to false' do 
+        it 'populates with nil values' do 
+          expect(hash.standardize!(:foo, :bar, :baz, :norf, :errors => false)).to eql(with_nil)
+        end
+
+        it 'doesn\'t raise an error' do 
+          expect{ hash.standardize!(:foo, :bar, :baz, :norf, :errors => false) }.not_to raise_error
+        end
+      end
+
+      context 'wtih errors option set to true' do 
+        it 'raises an error' do 
+          expect{ hash.standardize!(:foo, :bar, :baz, :norf, :errors => true) }.to raise_error(ArgumentError)
+        end
+      end
+    end
+  end
+end
